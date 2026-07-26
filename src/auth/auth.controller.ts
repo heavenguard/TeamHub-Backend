4124bd863e1d79f0509b98647e33ad566b8aaaa1
heavenguard <heavenguard237@gmail.com>
2026-07-27 00:36:36 +0100

    Refactor profile method to use Req and user.id

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,6 @@ import {
   Controller,
   Get,
   Post,
-  Request,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -52,7 +51,7 @@ export class AuthController {
   @ApiOperation({
     summary: 'Current authenticated user',
   })
-  profile(@Request() req: any) {
-    return this.authService.profile(req.user.userId);
+  profile(@Req() req: any) {
+    return this.authService.profile(req.user.id);
   }
 }
